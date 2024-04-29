1e818dd5d53683edf13b7d9ad4893556f4ebff6a
Guilherme Garcia <gui123.garcia@gmail.com>
2024-04-29 18:28:34 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Guilherme Garcia on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
